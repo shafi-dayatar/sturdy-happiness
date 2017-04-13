5f4d7bce461200521fad959add230adeb3b9d766
rentala <anudeep.rentala@gmail.com>
2017-04-12 17:25:12 -0700

    raft server rename

--- src/gash/router/server/states/RaftServerState.java
+++ src/gash/router/server/states/RaftServerState.java
@@ -10,29 +10,11 @@ import org.slf4j.LoggerFactory;
 /**
  *  Parent state class that defines every node - Leader Candidate Follower
  */
-public abstract class NodeState {
-    protected static Logger logger = LoggerFactory.getLogger("state");
-    public NodeState(){
+public interface RaftServerState {
 
-    }
-    //initial term is always 0
-    private int currentTerm = 0;
-    public void setCurrentTerm(int currentTerm){
-        this.currentTerm = currentTerm;
-    }
-    public int getCurrentTerm(){
-        return this.currentTerm;
-    }
-    //initial term is always 0
-    private int votedFor = 0;
-    public void setVotedFor(int votedFor){
-        this.votedFor = votedFor;
-    }
-    public int getVotedFor(){
-        return this.votedFor;
-    }
-    public void vote(){
-
-    }
-
+    public void requestVote();
+    public void startElection();
+    public void leaderElect();
+    public void logAppend();
+    public void collectVote();
 }
