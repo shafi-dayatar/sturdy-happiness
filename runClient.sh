d1638929eb5f7544dbb18e5e0d4146f35df7528a
prabhutejpulagam <prabhutej.pulagam@gmail.com>
2017-04-22 22:18:17 -0700

    added menu to client script

--- runClient.sh
+++ runClient.sh
@@ -12,11 +12,31 @@ echo server home = $SVR_HOME
 #cd ${SVR_HOME}
 
 JAVA_MAIN='gash.router.app.DemoApp'
-JAVA_ARGS="$1 $2 $3 $4 $5"
-#echo -e "\n** config: ${JAVA_ARGS} **\n"
+
+
 
 # superceded by http://ww.oracle.com/technetwork/java/tuning-139912.html
 JAVA_TUNE='-client -Djava.net.preferIPv4Stack=true'
 
+read -p "Enter the IP you want to connect to  " ip
+read -p "Enter the port to connect to  " port
+while true
+do
+read -p "1. Read a file   2. Write a file  " action
 
+if [ $action == "1" ]
+then 
+    action="get"
+elif [ $action == "2" ]
+then
+    action="post"
+fi
+
+read -p "Enter File Path  " path
+read -p "Enter File Name  " fname
+JAVA_ARGS="$ip $port $action $path "$fname""
+#echo -e "\n** config: ${JAVA_ARGS} **\n"
+#echo $ip,$port,$action,$path,$fname
 java ${JAVA_TUNE} -cp .:${SVR_HOME}/lib/'*':${SVR_HOME}/classes ${JAVA_MAIN} ${JAVA_ARGS} 
+
+done
